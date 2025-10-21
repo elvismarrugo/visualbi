f80a5cf102a509b128514dfaaa63365a2038491a
Elvix VBI Visual BI <elvix@somosvisualbi.com>
2025-10-21 19:41:04 +0000

    separalos un tantito

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Logo() {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center gap-1">
       <Image
         src="/logo.png"
         alt="Visual BI Logo"

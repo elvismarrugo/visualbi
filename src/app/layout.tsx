f7dd0bee3f2448077dff5c33afa37f50215ee56b
Elvix Marrugo <elvismarrugo@gmail.com>
2025-11-13 23:32:46 +0000

    el rayo tampoco era el logo, yo tenia mi propio logo, solo queria cambia

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'Soluciones Visual BI',
   description: 'Automatizando Su Éxito con Tecnología Inteligente.',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
